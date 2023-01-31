e767bf493cb421a6baf82320aebd4dde43cdc16e
yoichifay <124044138+yoichifay@users.noreply.github.com>
2023-01-31 19:24:43 +0900

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,20 +1,20 @@
 import { Configuration, OpenAIApi } from 'openai';
-
+ 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
-
+ 
 const openai = new OpenAIApi(configuration);
 const basePromptPrefix =
 `
-Write me a detailed table of contents for a blog post with the title below.
-
-Title:
+下記のタイトルのブログ記事の詳細な目次を書いてください。
+ 
+  タイトル:
 `
-
+ 
 const generateAction = async (req, res) => {
   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
-
+ 
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
     prompt: `${basePromptPrefix}${req.body.userInput}`,
@@ -23,17 +23,17 @@ const generateAction = async (req, res) => {
   });
   
   const basePromptOutput = baseCompletion.data.choices.pop();
-
+ 
   // I build Prompt #2.
   const secondPrompt = 
   `
-  Take the table of contents and title of the blog post below and generate a blog post written in thwe style of Paul Graham. Make it feel like a story. Don't just list the points. Go deep into each one. Explain why.
-
-  Title: ${req.body.userInput}
-
-  Table of Contents: ${basePromptOutput.text}
-
-  Blog Post:
+  以下のブログ記事の目次とタイトルを参考に、ポールグラハムの文体で書かれたブログ記事を作成してください。ストーリーのある文章にしてください。ポイントを列挙するだけではいけません。一つ一つを深く掘り下げて、なぜそうなるのかを説明してください。
+ 
+  タイトル: ${req.body.userInput}
+ 
+  目次: ${basePromptOutput.text}
+ 
+  ブログ:
   `
   
   // I call the OpenAI API a second time with Prompt #2
@@ -42,15 +42,15 @@ const generateAction = async (req, res) => {
     prompt: `${secondPrompt}`,
     // I set a higher temperature for this one. Up to you!
     temperature: 0.85,
-		// I also increase max_tokens.
+        // I also increase max_tokens.
     max_tokens: 1250,
   });
   
   // Get the output
   const secondPromptOutput = secondPromptCompletion.data.choices.pop();
-
+ 
   // Send over the Prompt #2's output to our UI instead of Prompt #1's.
   res.status(200).json({ output: secondPromptOutput });
 };
-
+ 
 export default generateAction;
